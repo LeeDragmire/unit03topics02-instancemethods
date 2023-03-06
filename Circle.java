5e294a12eddc4c73e6dd7e7ce569a14dd2fc871f
colton <adamscolton64@gmail.com>
2023-03-06 12:27:52 -0600

    Complete

--- Circle.java
+++ Circle.java
@@ -1,3 +1,51 @@
 public class Circle {
-    
+    private double x;
+    private double y;
+    private double rad;
+
+    public Circle(double xVal, double yVal, double radius) {
+        x = xVal;
+        y = yVal;
+        rad = radius;
+    }
+    public double getArea() {
+        return x * y * Math.pow(3.14, 2);
+    }
+    public double getCircumference() {
+        return 2 * 3.14 * rad;
+    }
+    public boolean intersects(Circle arg) {
+        boolean output = false;
+        if ((x + rad >= arg.x - arg.rad && y + rad >= arg.y - arg.rad) && 
+            (x - rad <= arg.x + arg.rad && y - rad <= arg.y + arg.rad) &&
+            !((x + rad > arg.x + arg.rad && y + rad > arg.y + arg.rad) && 
+            (x - rad < arg.x - arg.rad && y - rad < arg.y - arg.rad))) {
+            output = true;
+        }
+        return output;
+    }
+    public boolean contains(Circle arg) {
+        boolean output = false;
+        if ((x + rad > arg.x + arg.rad && y + rad > arg.y + arg.rad) && 
+            (x - rad < arg.x - arg.rad && y - rad < arg.y - arg.rad)) {
+                output = true;
+        }
+        return output;
+    }
+    public static void main(String[] args) {
+        Circle test = new Circle(0, 0, 5);
+        Circle test2 = new Circle(-2, -1, 5);
+        Circle test3 = new Circle(-2, -1, 2);
+        Circle error = new Circle(3, 0, 2);
+        System.out.println(test.getArea());
+        System.out.println(test2.getArea());
+        System.out.println(test.getCircumference());
+        System.out.println(test2.getCircumference());
+        System.out.println(test.intersects(test2));
+        System.out.println(test.contains(test2));
+        System.out.println(test.intersects(test3));
+        System.out.println(test.contains(test3));
+        System.out.println(test.intersects(error));
+        System.out.println(test.contains(error));
+    }
 }
--- Point2D.java
+++ Point2D.java
@@ -1,3 +1,40 @@
 public class Point2D {
-    
+    private double x;
+    private double y;
+
+    public Point2D(double xVal, double yVal) {
+        x = xVal;
+        y = yVal;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double distanceTo(Point2D arg) {
+        return Math.sqrt(Math.pow((arg.getX() - this.x), 2) + 
+            Math.pow((arg.getY() - this.y), 2)) ;
+    }
+    public String toString() {
+        String output = "(" + getX() + ", " + getY() + ")";
+        return output;
+    }
+
+    public static void main(String[] args) {
+        Point2D test = new Point2D(-2.5, 1);
+        Point2D test2 = new Point2D(5, 5);
+        System.out.println(test);
+        System.out.println(test.distanceTo(test2));
+        test.setX(2);
+        test.setY(5);
+        System.out.println(test);
+        System.out.println(test.distanceTo(test2));
+    }
 }
